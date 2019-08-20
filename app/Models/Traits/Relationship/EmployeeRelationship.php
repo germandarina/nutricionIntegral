066bc8abb62f20a8de98fca2399fe6ca5b86f7a7
German <german.exequiel.salinas@gmail.com>
2019-08-19 21:40:05 -0300

    migracion y relacion entre pacientes y empleados

--- app/Models/Traits/Relationship/EmployeeRelationship.php
+++ app/Models/Traits/Relationship/EmployeeRelationship.php
@@ -2,10 +2,16 @@
 
 namespace App\Models\Traits\Relationship;
 
+use App\Models\Patient;
+
 /**
  * Class EmployeeRelationship.
  */
 trait EmployeeRelationship
 {
 
+    public function patients()
+    {
+        return $this->belongsToMany(Patient::class, 'employee_patient')->withTimestamps();
+    }
 }
--- app/Models/Traits/Relationship/PatientRelationship.php
+++ app/Models/Traits/Relationship/PatientRelationship.php
@@ -4,10 +4,15 @@ namespace App\Models\Traits\Relationship;
 
 
 
+use App\Models\Employee;
+
 /**
  * Class PatientRelationship.
  */
 trait PatientRelationship
 {
-    
+    public function employees()
+    {
+        return $this->belongsToMany(Employee::class, 'employee_patient')->withTimestamps();
+    }
 }
