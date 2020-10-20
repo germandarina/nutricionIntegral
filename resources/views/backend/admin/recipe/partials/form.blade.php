29345177931b0840fe5390e44ec875ebfe85b2df
German Salinas <german.exequiel.salinas@gmail.com>
2020-10-20 09:07:10 -0300

    correccion modal observacion y agregar ingrediente

--- resources/views/backend/admin/recipe/partials/form.blade.php
+++ resources/views/backend/admin/recipe/partials/form.blade.php
@@ -47,30 +47,3 @@
             <a href="#" class="btn btn-success btn-xs btn-block" onclick="modalObservation(event)">Nueva Observación</a>
         </div>
     </div>
-
-    @section('modal-yield')
-        <div class="modal fade" id="modalObservation" role="dialog" aria-labelledby="myModalLabel" style="display: none;" aria-hidden="true">
-            <div class="modal-dialog modal-lg" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h4 class="modal-title">Nueva Observación</h4>
-                        <button class="close" type="button" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                    </div>
-                    <div class="modal-body" id="bodyIngredients">
-                        <div class="row mt-3">
-                            <div class="col-sm-2">
-                                <label for="">Nombre</label>
-                            </div>
-                            <div class="col-sm-10">
-                                <input type="text" class="form-control" id="name_observation" required>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cerrar</button>
-                        <button id="btnObservation" class="btn btn-primary" type="button" onclick="storeObservation(event)">Guardar</button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    @endsection
