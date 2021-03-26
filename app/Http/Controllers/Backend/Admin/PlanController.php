4897f5124d4e30c6b5ced52b548eb14bcd0d8b09
German Salinas <german.exequiel.salinas@gmail.com>
2021-03-25 22:38:40 -0300

    fix nombre de planes al exportar

--- app/Http/Controllers/Backend/Admin/PlanController.php
+++ app/Http/Controllers/Backend/Admin/PlanController.php
@@ -521,8 +521,9 @@ class PlanController extends Controller
                                                                             'color_observations','color_headers'));
         }
 
-        $nombre_plan       = strtolower(trim($plan->name));
-        $nombre_archivo    = snake_case("{$nombre_plan}_{$patient->full_name}");
+        $nombre_plan       = str_replace(strtolower(trim($plan->name)),',','_') ;
+        $nombre_patient    = str_replace($patient->full_name,',','_');
+        $nombre_archivo    = snake_case("{$nombre_plan}_{$nombre_patient}");
 
         if(request('word'))
         {
